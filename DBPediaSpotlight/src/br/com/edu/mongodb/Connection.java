b4eeb0a8a46d5d0aa96b1120f179f4cdf0977222
nevessangelo <nevessangelo@gmail.com>
2016-08-24 19:51:50 -0300

    Buscando o void através do mongoDB da base de dados extraída pelo Crawlei.

--- DBPediaSpotlight/src/br/com/edu/mongodb/Connection.java
+++ DBPediaSpotlight/src/br/com/edu/mongodb/Connection.java
@@ -5,8 +5,16 @@
  */
 package br.com.edu.mongodb;
 
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import java.util.ArrayList;
+import java.util.List;
+import org.bson.Document;
 
 /**
  *
@@ -16,9 +24,33 @@ public class Connection {
 
     public static void main(String[] args) {
         MongoClient mongoClient = new MongoClient();
-        MongoDatabase db = mongoClient.getDatabase("test");
+        MongoDatabase db = mongoClient.getDatabase("data_catalog");
         System.out.println(db.getName());
+        
+       for (String name : db.listCollectionNames())
+            System.out.println(name);
+       
+        System.out.println("Pegando collection dos voids...");
+        MongoCollection coll = db.getCollection("voids");
+        
+        System.out.println("Mostrando objetos..");
+        
+        try (MongoCursor<Document> cur = coll.find().iterator()) {
+            while (cur.hasNext()) {
+                Document doc = cur.next();
+                List list = new ArrayList(doc.values());
+                System.out.print(list.get(1));
+                System.out.print(": ");
+                System.out.println(list.get(2));
+            }
+            
+        }
 
+        
+        
+       // System.out.println(coll.toString());
+
+        
+        mongoClient.close();
     }
-
 }
