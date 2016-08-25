969afebfed3d3ce5656db2b54ad67db274e9f127
Luiz Leme <lapaesleme@gmail.com>
2016-08-25 20:52:25 -0300

    ok

--- DataCrawler2/src/ic/uff/utils/Query.java
+++ DataCrawler2/src/ic/uff/utils/Query.java
@@ -1,13 +1,29 @@
 package ic.uff.utils;
 
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.WebContent;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+
 public class Query {
-	
-	public static void main(String[] args){
-		// Input: data_catalog.datasets (MongoDB)
-		// Output: dssearch.datasets_temp (MongoDB)
-		// Aux: dssearch.matchings_temp (MongoDB)
-		// TODO Topic extraction procedure 
-		System.out.println("OK");
+
+	public static ResultSet exeSelect(String sparqlEndPoint, String queryString, int limit, int offset) {
+		try (QueryExecution exec = new QueryEngineHTTP(sparqlEndPoint, queryString)) {
+			((QueryEngineHTTP) exec).setModelContentType(WebContent.contentTypeRDFXML);
+			return exec.execSelect();
+		} catch (Exception e) {
+		}
+		return null;
+	}
+
+	public static Model execConstruct(String sparqlEndPoint, String queryString, int limit, int offset) {
+		try (QueryExecution exec = new QueryEngineHTTP(sparqlEndPoint, queryString)) {
+			((QueryEngineHTTP) exec).setModelContentType(WebContent.contentTypeRDFXML);
+			return exec.execConstruct();
+		} catch (Exception e) {
+		}
+		return null;
 	}
 
 }
