22140d59304b528537fec4c7a0a8348db476f726
Luiz Leme <lapaesleme@gmail.com>
2016-08-29 23:39:20 -0300

    ok

--- DataCrawler2/src/main/java/uff/ic/swlab/datacrawler/Test1.java
+++ DataCrawler2/src/main/java/uff/ic/swlab/datacrawler/Test1.java
@@ -1,9 +1,9 @@
 package uff.ic.swlab.datacrawler;
 
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.riot.Lang;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import uff.ic.swlab.utils.SparqlServer;
 import uff.ic.swlab.utils.VoID;
 
 public class Test1 {
@@ -14,8 +14,13 @@ public class Test1 {
         String[] sparqls = {"http://acm.rkbexplorer.com/sparql"};
         String[] urls = {"http://acm.rkbexplorer.com/id/999"};
         Model model = VoID.getVoID(sparqls, urls);
-        model.write(System.out, Lang.TURTLE.getName());
-        System.out.println("Done.");
 
+        String name_uri = "https://datahub.io/api/rest/dataset/rkb-explorer-acm";
+        SparqlServer server = new SparqlServer();
+        server.dataURL = "http://localhost:8080/fuseki/void/data";
+        server.putModel(name_uri, model);
+
+        //model.write(System.out, Lang.TURTLE.getName());
+        System.out.println("Done.");
     }
 }
