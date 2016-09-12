1e01d5a026c3c3e015bc9b1122297edacf72bb91
Luiz Leme <lapaesleme@gmail.com>
2016-09-12 17:29:09 -0300

    ok

--- Dataset Crawler/src/main/java/uff/ic/swlab/datacrawler/Main.java
+++ Dataset Crawler/src/main/java/uff/ic/swlab/datacrawler/Main.java
@@ -4,7 +4,11 @@ import java.net.MalformedURLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import org.apache.http.client.HttpClient;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.web.HttpOp;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import uff.ic.swlab.common.util.Resource;
@@ -24,19 +28,24 @@ public class Main {
         Logger.getRootLogger().setLevel(Level.OFF);
         System.out.println("Crawler started.");
 
-        // final HttpClient httpclient = new DefaultHttpClient();
-        // final HttpParams params = httpclient.getParams();
-        // params.setParameter(HttpConnectionParams.CONNECTION_TIMEOUT, 10000);
-        // params.setParameter(HttpConnectionParams.SO_TIMEOUT, 60000);
-        // HttpOp.setDefaultHttpClient(httpclient);
-        try (Crawler<Dataset> crawler = new CatalogCrawler();) {
+        final HttpClient httpclient = HttpOp.createCachingHttpClient();
+        final HttpParams params = httpclient.getParams();
+        params.setParameter(HttpConnectionParams.CONNECTION_TIMEOUT, 10000);
+        params.setParameter(HttpConnectionParams.SO_TIMEOUT, 30000);
+        HttpOp.setDefaultHttpClient(httpclient);
 
-            SparqlServer server = new SparqlServer();
-            server.dataURL = "http://localhost:8080/fuseki/void/data";
-            server.sparqlURL = "http://localhost:8080/fuseki/void/sparql";
+        String fuseki = "http://localhost:8080/fuseki/void2";
+        String catalog = "http://linkeddatacatalog.dws.informatik.uni-mannheim.de";
+
+        SparqlServer server = new SparqlServer();
+        server.dataURL = fuseki + "/data";
+        server.updateURL = fuseki + "/update";
+        server.sparqlURL = fuseki + "/sparql";
+
+        try (Crawler<Dataset> crawler = new CatalogCrawler(catalog);) {
+
             //List<String> graphNames = server.listGraphNames();
-
-            ExecutorService pool = Executors.newWorkStealingPool(20);
+            ExecutorService pool = Executors.newWorkStealingPool(50);
             while (crawler.hasNext()) {
                 Dataset dataset = crawler.next();
 
