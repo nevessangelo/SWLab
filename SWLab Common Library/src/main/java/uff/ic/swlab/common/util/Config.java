bb9712701dd83afcadc2ae1898388f36ce95bb2f
Luiz Leme <lapaesleme@gmail.com>
2016-09-15 21:20:12 -0300

    ok

--- SWLab Common Library/src/main/java/uff/ic/swlab/common/util/Config.java
+++ SWLab Common Library/src/main/java/uff/ic/swlab/common/util/Config.java
@@ -8,14 +8,11 @@ public class Config {
     public static Integer TASK_INSTANCES = 50;
     public static Integer PARALLELISM = 30;
     public static Long SPARQL_TIMEOUT = 30000l;
-    public static Long MODEL_READ_TIMEOUT = 10000l;
+    public static Long MODEL_READ_TIMEOUT = 30000l;
     public static Long TASK_RUNNING_TIMEOUT = 300000l;
-    public static Integer CONNECTION_TIMEOUT = 10000;
-    public static Integer SO_TIMEOUT = 30000;
     public static Integer POOL_SHUTDOWN_TIMEOUT = 1;
 
-    private Config() {
-
-    }
+    public static Integer CONNECTION_TIMEOUT = 10000;
+    public static Integer SO_TIMEOUT = 30000;
 
 }
