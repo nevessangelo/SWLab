4d959744f25af855e0315f0b55d0478545d41f46
lapaesleme <lapaesleme@gmail.com>
2017-05-21 10:56:19 -0300

    ok

--- EntityRelatedness/src/main/java/uff/ic/lleme/entityrelatedness/Main.java
+++ EntityRelatedness/src/main/java/uff/ic/lleme/entityrelatedness/Main.java
@@ -16,122 +16,158 @@ import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.vocabulary.DCTerms;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.vocabulary.VOID;
 import org.apache.jena.vocabulary.XSD;
 
 public class Main {
 
+    private static String ontologyNS = "http://swlab.ic.uff.br/ontologyEntityRelatednessTestDataset.rdf#";
+    private static String dataNS = "http://swlab.ic.uff.br/resource/";
+    private static String voidNS = "http://swlab.ic.uff.br/void.ttl#";
+    private static String alignNS = "http://knowledgeweb.semanticweb.org/heterogeneity/alignment#";
+
+    private static String localDir = "./data/EntityRelatednessTestDataset";
+    private static String datasetName = "EntityRelatednessTestData";
+    private static String fusekiURL = "http://swlab.ic.uff.br/fuseki";
+
+    private static MusicScores musicScores = new MusicScores();
+    private static MusicRankedPaths musicRankedPaths = new MusicRankedPaths();
+    private static MusicPropertyRelevanceScore musicPropertyRelevanceScore = new MusicPropertyRelevanceScore();
+    private static MusicEntityMappings musicEntityMapping = new MusicEntityMappings();
+    private static MovieScores movieScores = new MovieScores();
+    private static MovieRankedPaths movieRankedPaths = new MovieRankedPaths();
+    private static MoviePropertyRelevanceScore moviePropertyRelevanceScore = new MoviePropertyRelevanceScore();
+    private static MovieEntityMappings movieEntityMapping = new MovieEntityMappings();
+    private static MusicClassMapping musicClassMapping = new MusicClassMapping();
+    private static MovieClassMapping movieClassMapping = new MovieClassMapping();
+    private static MovieEntityPairs movieEntityPairs = new MovieEntityPairs();
+    private static MusicEntityPairs musicEntityPairs = new MusicEntityPairs();
+
     public static void main(String[] args) throws FileNotFoundException, IOException {
-        MusicEntityPairs musicEntityPairs = new MusicEntityPairs();
-        MovieEntityPairs movieEntityPairs = new MovieEntityPairs();
-        MovieClassMapping movieClassMapping = new MovieClassMapping();
-        MusicClassMapping musicClassMapping = new MusicClassMapping();
-
-        MovieEntityMappings movieEntityMapping = new MovieEntityMappings();
-        MoviePropertyRelevanceScore moviePropertyRelevanceScore = new MoviePropertyRelevanceScore();
-        MovieRankedPaths movieRankedPaths = new MovieRankedPaths();
-        MovieScores movieScores = new MovieScores();
-
-        MusicEntityMappings musicEntityMapping = new MusicEntityMappings();
-        MusicPropertyRelevanceScore musicPropertyRelevanceScore = new MusicPropertyRelevanceScore();
-        MusicRankedPaths musicRankedPaths = new MusicRankedPaths();
-        MusicScores musicScores = new MusicScores();
-
-        String alignns = "http://knowledgeweb.semanticweb.org/heterogeneity/alignment#";
-        String gtns = "http://swlab.ic.uff.br/resource/";
-        String erns = "http://swlab.ic.uff.br/ontology/EntityRelatednessTestDataset.rdf#";
-
-        {
-            Model ontology = ModelFactory.createDefaultModel();
-            ontology.setNsPrefix("", erns);
-
-            Resource pathElementClass = ontology.createResource(erns + "PathElement", RDFS.Class);
-            Property score = ontology.createProperty(erns + "score");
-            score.addProperty(RDFS.domain, pathElementClass);
-            score.addProperty(RDFS.range, XSD.xdouble);
-            Resource entityClass = ontology.createResource(erns + "Entity", RDFS.Class);
-            entityClass.addProperty(RDFS.subClassOf, pathElementClass);
-            Resource propertyClass = ontology.createResource(erns + "Property", RDFS.Class);
-            propertyClass.addProperty(RDFS.subClassOf, pathElementClass);
-
-            Resource entityPairClass = ontology.createResource(erns + "EntityPair", RDFS.Class);
-            Property first = ontology.createProperty(erns + "first");
-            Property second = ontology.createProperty(erns + "second");
-            first.addProperty(RDFS.domain, entityPairClass);
-            first.addProperty(RDFS.range, entityClass);
-            second.addProperty(RDFS.domain, entityPairClass);
-            second.addProperty(RDFS.range, entityClass);
-
-            Property hasPath = ontology.createProperty(erns + "hasPath");
-            hasPath.addProperty(RDFS.domain, entityPairClass);
-            hasPath.addProperty(RDFS.range, ontology.createResource(erns + "Path", RDFS.Class));
-
-            Resource pathClass = ontology.createResource(erns + "Path", RDFS.Class);
-            score.addProperty(RDFS.domain, pathClass);
-            Property rank = ontology.createProperty(erns + "rank");
-            rank.addProperty(RDFS.domain, pathClass);
-            rank.addProperty(RDFS.range, XSD.xlong);
-            Property expression = ontology.createProperty(erns + "expression");
-            expression.addProperty(RDFS.domain, pathClass);
-            expression.addProperty(RDFS.range, XSD.xstring);
-
-            Property hasListOfPathElements = ontology.createProperty(erns + "hasListOfpathElements");
-            hasListOfPathElements.addProperty(RDFS.domain, pathClass);
-            hasListOfPathElements.addProperty(RDFS.range, ontology.createResource(erns + "ListOfPathElements", RDFS.Class));
-
-            Resource listOfPathElementsClass = ontology.createResource(erns + "ListOfPathElements", RDFS.Class);
-            listOfPathElementsClass.addProperty(RDFS.subClassOf, RDF.List);
-            Property firstPathElement = ontology.createProperty(erns + "firstPathElement");
-            firstPathElement.addProperty(RDFS.subPropertyOf, RDF.first);
-            firstPathElement.addProperty(RDFS.domain, listOfPathElementsClass);
-            firstPathElement.addProperty(RDFS.range, pathElementClass);
-            Property restOfPathElements = ontology.createProperty(erns + "restOfPathElements");
-            restOfPathElements.addProperty(RDFS.subPropertyOf, RDF.rest);
-            restOfPathElements.addProperty(RDFS.domain, listOfPathElementsClass);
-            restOfPathElements.addProperty(RDFS.range, listOfPathElementsClass);
-
-            (new File("./data/EntityRelatednessTestDataset/RDF/Ontology")).mkdirs();
-            OutputStream out0 = new FileOutputStream(new File("./data/EntityRelatednessTestDataset/RDF/Ontology/EntityRelatednessTestDataset.rdf"));
-            RDFDataMgr.write(out0, ontology, Lang.RDFXML);
-        }
-
-        {
-            Model dataset = ModelFactory.createDefaultModel();
-            dataset.setNsPrefix("", gtns);
-            dataset.setNsPrefix("align", alignns);
-
-            Resource alignmentClass = dataset.createResource(alignns + "Alignment");
-            Resource alignment = dataset.createResource(alignmentClass);
-
-            for (Map.Entry<String, ArrayList<Pair>> entry : movieEntityMapping.entrySet())
-                for (Pair pair : entry.getValue()) {
-                    Resource cell = dataset.createResource(erns + pair.label, dataset.createResource(alignns + "Cell"));
-                    cell.addProperty(dataset.createProperty(alignns + "entity1"), pair.entity1);
-                    cell.addProperty(dataset.createProperty(alignns + "entity2"), pair.entity2);
-                    alignment.addProperty(dataset.createProperty(alignns + "map"), cell.as(RDFNode.class));
-                }
-
-            for (Map.Entry<String, ArrayList<Pair>> entry : musicEntityMapping.entrySet())
-                for (Pair pair : entry.getValue()) {
-                    Resource cell = dataset.createResource(erns + pair.label, dataset.createResource(alignns + "Cell"));
-                    cell.addProperty(dataset.createProperty(alignns + "entity1"), pair.entity1);
-                    cell.addProperty(dataset.createProperty(alignns + "entity2"), pair.entity2);
-                    alignment.addProperty(dataset.createProperty(alignns + "map"), cell.as(RDFNode.class));
-                }
-
-            OutputStream out = new FileOutputStream(new File("./data/EntityRelatednessTestDataset/RDF/EntityRelatednessTestDataset.ttl"));
-            RDFDataMgr.write(out, dataset, Lang.TURTLE);
-
-            OutputStream out2 = new FileOutputStream(new File("./data/EntityRelatednessTestDataset/RDF/EntityRelatednessTestDataset.rdf"));
-            RDFDataMgr.write(out2, dataset, Lang.RDFXML);
-
-            OutputStream out3 = new FileOutputStream(new File("./data/EntityRelatednessTestDataset/RDF/EntityRelatednessTestDataset.json"));
-            RDFDataMgr.write(out3, dataset, Lang.JSONLD);
-
-            DatasetAccessor accessor = DatasetAccessorFactory.createHTTP("http://swlab.ic.uff.br/fuseki/EntityRelatednessTestDataset/data");
-            accessor.putModel(dataset);
-        }
+        createOntology();
+        createDataset();
+        createVoID();
         System.out.println("Fim.");
     }
+
+    private static void createOntology() throws FileNotFoundException {
+        Model ontology = ModelFactory.createDefaultModel();
+        ontology.setNsPrefix("", ontologyNS);
+
+        Resource pathElementClass = ontology.createResource(ontologyNS + "PathElement", RDFS.Class);
+        Property score = ontology.createProperty(ontologyNS + "score");
+        score.addProperty(RDFS.domain, pathElementClass);
+        score.addProperty(RDFS.range, XSD.xdouble);
+        Resource entityClass = ontology.createResource(ontologyNS + "Entity", RDFS.Class);
+        entityClass.addProperty(RDFS.subClassOf, pathElementClass);
+        Resource propertyClass = ontology.createResource(ontologyNS + "Property", RDFS.Class);
+        propertyClass.addProperty(RDFS.subClassOf, pathElementClass);
+
+        Resource entityPairClass = ontology.createResource(ontologyNS + "EntityPair", RDFS.Class);
+        Property first = ontology.createProperty(ontologyNS + "first");
+        Property second = ontology.createProperty(ontologyNS + "second");
+        first.addProperty(RDFS.domain, entityPairClass);
+        first.addProperty(RDFS.range, entityClass);
+        second.addProperty(RDFS.domain, entityPairClass);
+        second.addProperty(RDFS.range, entityClass);
+
+        Property hasPath = ontology.createProperty(ontologyNS + "hasPath");
+        hasPath.addProperty(RDFS.domain, entityPairClass);
+        hasPath.addProperty(RDFS.range, ontology.createResource(ontologyNS + "Path", RDFS.Class));
+
+        Resource pathClass = ontology.createResource(ontologyNS + "Path", RDFS.Class);
+        score.addProperty(RDFS.domain, pathClass);
+        Property rank = ontology.createProperty(ontologyNS + "rank");
+        rank.addProperty(RDFS.domain, pathClass);
+        rank.addProperty(RDFS.range, XSD.xlong);
+        Property expression = ontology.createProperty(ontologyNS + "expression");
+        expression.addProperty(RDFS.domain, pathClass);
+        expression.addProperty(RDFS.range, XSD.xstring);
+
+        Property hasListOfPathElements = ontology.createProperty(ontologyNS + "hasListOfpathElements");
+        hasListOfPathElements.addProperty(RDFS.domain, pathClass);
+        hasListOfPathElements.addProperty(RDFS.range, ontology.createResource(ontologyNS + "ListOfPathElements", RDFS.Class));
+
+        Resource listOfPathElementsClass = ontology.createResource(ontologyNS + "ListOfPathElements", RDFS.Class);
+        listOfPathElementsClass.addProperty(RDFS.subClassOf, RDF.List);
+        Property firstPathElement = ontology.createProperty(ontologyNS + "firstPathElement");
+        firstPathElement.addProperty(RDFS.subPropertyOf, RDF.first);
+        firstPathElement.addProperty(RDFS.domain, listOfPathElementsClass);
+        firstPathElement.addProperty(RDFS.range, pathElementClass);
+        Property restOfPathElements = ontology.createProperty(ontologyNS + "restOfPathElements");
+        restOfPathElements.addProperty(RDFS.subPropertyOf, RDF.rest);
+        restOfPathElements.addProperty(RDFS.domain, listOfPathElementsClass);
+        restOfPathElements.addProperty(RDFS.range, listOfPathElementsClass);
+
+        (new File(localDir + "/RDF/Ontology")).mkdirs();
+        OutputStream out = new FileOutputStream(new File(localDir + "/RDF/Ontology/" + datasetName + ".rdf"));
+        RDFDataMgr.write(out, ontology, Lang.RDFXML);
+    }
+
+    private static void createDataset() throws FileNotFoundException {
+        Model dataset = ModelFactory.createDefaultModel();
+        dataset.setNsPrefix("", dataNS);
+        dataset.setNsPrefix("align", alignNS);
+
+        Resource alignmentClass = dataset.createResource(alignNS + "Alignment");
+        Resource alignment = dataset.createResource(alignmentClass);
+
+        for (Map.Entry<String, ArrayList<Pair>> entry : movieEntityMapping.entrySet())
+            for (Pair pair : entry.getValue()) {
+                Resource cell = dataset.createResource(ontologyNS + pair.label, dataset.createResource(alignNS + "Cell"));
+                cell.addProperty(dataset.createProperty(alignNS + "entity1"), pair.entity1);
+                cell.addProperty(dataset.createProperty(alignNS + "entity2"), pair.entity2);
+                cell.addProperty(dataset.createProperty(alignNS + "relation"), "=");
+                alignment.addProperty(dataset.createProperty(alignNS + "map"), cell.as(RDFNode.class));
+            }
+
+        for (Map.Entry<String, ArrayList<Pair>> entry : musicEntityMapping.entrySet())
+            for (Pair pair : entry.getValue()) {
+                Resource cell = dataset.createResource(ontologyNS + pair.label, dataset.createResource(alignNS + "Cell"));
+                cell.addProperty(dataset.createProperty(alignNS + "entity1"), pair.entity1);
+                cell.addProperty(dataset.createProperty(alignNS + "entity2"), pair.entity2);
+                cell.addProperty(dataset.createProperty(alignNS + "relation"), "=");
+                alignment.addProperty(dataset.createProperty(alignNS + "map"), cell.as(RDFNode.class));
+            }
+
+        (new File(localDir + "/RDF")).mkdirs();
+        OutputStream out = new FileOutputStream(new File(localDir + "/RDF/" + datasetName + ".ttl"));
+        RDFDataMgr.write(out, dataset, Lang.TURTLE);
+
+        OutputStream out2 = new FileOutputStream(new File(localDir + "/RDF/" + datasetName + ".rdf"));
+        RDFDataMgr.write(out2, dataset, Lang.RDFXML);
+
+        OutputStream out3 = new FileOutputStream(new File(localDir + "/RDF/" + datasetName + ".json"));
+        RDFDataMgr.write(out3, dataset, Lang.JSONLD);
+
+        DatasetAccessor accessor = DatasetAccessorFactory.createHTTP(fusekiURL + "/" + datasetName + "/data");
+        accessor.putModel(dataset);
+    }
+
+    private static void createVoID() throws FileNotFoundException {
+        Model voidVocab = ModelFactory.createDefaultModel();
+        voidVocab.setNsPrefix("", dataNS);
+        voidVocab.setNsPrefix("align", alignNS);
+        voidVocab.read("http://vocab.deri.ie/void#");
+
+        Model voidData = ModelFactory.createDefaultModel();
+        voidData.createResource(ontologyNS + "EntityRelatednessTestData", VOID.Dataset)
+                .addProperty(DCTerms.description, "The entity relatedness problem refers to the question of "
+                        + "computing the relationship paths that better describe the connectivity between a "
+                        + "given entity pair. This dataset supports the evaluation of approaches that address "
+                        + "the entity relatedness problem. It covers two familiar domains, music and movies, and "
+                        + "uses data available in IMDb and last.fm, which are popular reference datasets in these "
+                        + "domains. The dataset contains 20 entity pairs from each of these domains and, for each "
+                        + "entity pair, a ranked list with 50 relationship paths. It also contains entity ratings "
+                        + "and property relevance scores for the entities and properties used in the paths.");
+
+        (new File(localDir + "/RDF")).mkdirs();
+        OutputStream out = new FileOutputStream(new File(localDir + "/RDF/void.ttl"));
+        RDFDataMgr.write(out, voidData, Lang.TURTLE);
+
+    }
+
 }
