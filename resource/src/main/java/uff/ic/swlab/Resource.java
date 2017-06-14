58d13b7a9b19eb3ff14647f60e116f3c048f30dc
Luiz Leme <lapaesleme@gmail.com>
2017-06-14 17:04:10 -0300

    ok

--- resource/src/main/java/uff/ic/swlab/Resource.java
+++ resource/src/main/java/uff/ic/swlab/Resource.java
@@ -51,7 +51,7 @@ public class Resource extends HttpServlet {
                 response.setHeader("Location", url);
                 response.sendRedirect(url);
             } else {
-                response.setContentType(lang.getContentType().getContentType());
+                response.setContentType(lang.getContentType());
                 RDFDataMgr.write(httpReponse, model, lang);
             }
             httpReponse.flush();
