05bf68af6a15bbe35eebdc8fed72a806e05af350
lapaesleme <lapaesleme@gmail.com>
2017-06-18 14:20:04 -0300

    ok

--- EntityRelatednessTestData/src/main/java/uff/ic/swlab/dataset/entityrelatednesstestdata/v3/Config.java
+++ EntityRelatednessTestData/src/main/java/uff/ic/swlab/dataset/entityrelatednesstestdata/v3/Config.java
@@ -10,10 +10,7 @@ public class Config {
 
     public static final String DATA_ROOT = "./resources/data/v3/raw";
     public static final String RDF_ROOT = "./resources/data/v3/rdf";
-    public static final String RDF_ROOT_0 = "../../EntityRelatednessTestData";
     public static final String DATASET_NAME = "EntityRelatednessTestData";
-
-    public static final String DATASET_NS = SWLabServer.BASE_URL + "resource/";
 
     public static final String LOCAL_ONTOLOGY_HOMEPAGE = RDF_ROOT + "/ontology/" + DATASET_NAME + "_v1/index.jsp";
     public static final String LOCAL_ONTOLOGY_NAME = RDF_ROOT + "/ontology/" + DATASET_NAME + "_v1.rdf.gz";
@@ -26,6 +23,7 @@ public class Config {
     public static String USERNAME = null;
     public static String PASSWORD = null;
     public static int PORT = 0;
+
     public static final String REMOTE_ONTOLOGY_HOMEPAGE = "/tomcat/ontology/" + DATASET_NAME + "_v1/index.jsp";
     public static final String REMOTE_ONTOLOGY_NAME = "/tomcat/ontology/" + DATASET_NAME + "_v1.rdf.gz";
     public static final String REMOTE_DATASET_HOMEPAGE = "/tomcat/dataset/" + DATASET_NAME + "_v3/index.jsp";
@@ -34,8 +32,7 @@ public class Config {
     public static final String REMOTE_JSON_DUMP_NAME = "/tomcat/dataset/" + DATASET_NAME + "_v3.json.gz";
     public static final String REMOTE_NTRIPLES_DUMP_NAME = "/tomcat/dataset/" + DATASET_NAME + "_v3.nt.gz";
 
-    public static final String FUSEKI_URL = SWLabServer.BASE_URL + "fuseki";
-    public static final String DATASET_URL = FUSEKI_URL + "/" + DATASET_NAME + "_v3/data";
+    public static final String FUSEKI_URL = SWLabServer.BASE_URL + "fuseki/" + DATASET_NAME + "_v3/data";
 
     public static void configure(String file) throws IOException {
         try (InputStream input = new FileInputStream(file);) {
