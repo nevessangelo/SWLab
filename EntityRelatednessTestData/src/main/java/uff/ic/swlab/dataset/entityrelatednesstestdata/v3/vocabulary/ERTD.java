08da9ff664959144f82f860f745ca3e9e6b9c943
Luiz Leme <lapaesleme@gmail.com>
2017-06-19 09:05:05 -0300

    ok

--- EntityRelatednessTestData/src/main/java/uff/ic/swlab/dataset/entityrelatednesstestdata/v3/vocabulary/ERTD.java
+++ EntityRelatednessTestData/src/main/java/uff/ic/swlab/dataset/entityrelatednesstestdata/v3/vocabulary/ERTD.java
@@ -1,10 +1,10 @@
 package uff.ic.swlab.dataset.entityrelatednesstestdata.v3.vocabulary;
 
-import uff.ic.swlab.SWLabServer;
+import uff.ic.swlab.SWLabHost;
 
 public class ERTD {
 
     public static final String PREFIX = "ertd";
-    public static final String NS = SWLabServer.BASE_URL + "resource/";
+    public static final String NS = SWLabHost.BASE_URL + "resource/";
 
 }
