b9cf7d8cb048b9ec6fc8de7305a064d8c42c4eee
lapaesleme <lapaesleme@gmail.com>
2017-06-29 16:52:45 -0300

    ok

---
+++ .well-known/src/main/java/uff/ic/swlab/VoID.java
@@ -0,0 +1,30 @@
+package uff.ic.swlab;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class VoID extends HttpServlet {
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = "/void.ttl";
+        response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+        response.setHeader("Location", url);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().write("Error: POST method not implemented.");
+    }
+}
